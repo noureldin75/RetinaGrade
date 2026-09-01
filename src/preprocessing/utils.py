379c3826c64bf16be5c875449d3a16d26874cb25
Youssef Ashraf <youseefashraf54@gmail.com>
2026-09-02 02:51:31 +0300

    .

--- src/preprocessing/utils.py
+++ src/preprocessing/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from PIL import Image
-
+from sklearn.model_selection import train_test_split
 def load_data(df, image_dir):
     """
     Load images and labels from a DataFrame and an image directory.
@@ -28,4 +28,18 @@ def load_data(df, image_dir):
     x = np.array(x)
     y = np.array(y)
     return x,y
-    
+
+def data_split(x, y, val_size=0.15, random_state=42):
+    """
+    Split the dataset into training and validation sets.
+
+    Parameters:
+    x (np.ndarray): Array of images.
+    y (np.ndarray): Array of labels.
+    val_size (float): Proportion of the dataset to include in the validation split.
+    random_state (int): Random seed for reproducibility.
+
+    Returns:
+    tuple: A tuple containing training and validation sets - (x_train, x_val, y_train, y_val).
+    """
+    return train_test_split(x, y, test_size=val_size, random_state=random_state, shuffle=True)    
