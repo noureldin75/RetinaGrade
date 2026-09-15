79c8389414b7bfe2efb27a8370a54792faba8555
Youssef Ashraf <youseefashraf54@gmail.com>
2026-09-15 17:22:12 +0300

    sorry

--- src/preprocessing/model_builder.py
+++ src/preprocessing/model_builder.py
@@ -4,13 +4,13 @@ import torchvision.models as models
 from custom_layers import GeM
 def build_model(model_name="efficientnet_b3", num_classes=5, freeze_features=True):
     model_func = getattr(models, model_name)
-    model = model_func(Weights="DEFAULT")
+    model = model_func(weights="DEFAULT")
     if freeze_features:
         for param in model.parameters():
             param.requires_grad = False
 
 
-    
+
 
     model.avgpool = GeM()
 
