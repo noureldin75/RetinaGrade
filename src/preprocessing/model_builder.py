ebbd0461152b49f82985405618218029d8d2775e
Youssef Ashraf <youseefashraf54@gmail.com>
2026-09-15 17:25:22 +0300

    Update model_builder.py

--- src/preprocessing/model_builder.py
+++ src/preprocessing/model_builder.py
@@ -4,7 +4,12 @@ import torchvision.models as models
 from custom_layers import GeM
 def build_model(model_name="efficientnet_b3", num_classes=5, freeze_features=True):
     model_func = getattr(models, model_name)
-    model = model_func(weights="DEFAULT")
+    try:
+        model = model_func(weights="DEFAULT")
+    except TypeError:
+        # Fallback for torchvision < 0.13
+        model = model_func(pretrained=True)
+        
     if freeze_features:
         for param in model.parameters():
             param.requires_grad = False
